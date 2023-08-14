b1132ff5e8493d109ea7cb90f0bfbd50d8e34291
Max <mikimak237@gmail.com>
2023-08-15 00:52:10 +0300

    AOP logging

--- src/main/java/com/example/demowithtests/util/annotations/LoggingServiceClassesAspect.java
+++ src/main/java/com/example/demowithtests/util/annotations/LoggingServiceClassesAspect.java
@@ -2,12 +2,14 @@ package com.example.demowithtests.util.annotations;
 
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.stat.Statistics;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 import static com.example.demowithtests.util.annotations.LogColorConstants.ANSI_BLUE;
@@ -18,14 +20,27 @@ import static com.example.demowithtests.util.annotations.LogColorConstants.ANSI_
 @Component
 public class LoggingServiceClassesAspect {
 
-    @Pointcut("execution(public * com.example.demowithtests.service.EmployeeServiceBean.*(..))")
+    private LocalDateTime startTime;
+    private final Statistics hibernateStatistics;
+
+    @Autowired
+    public LoggingServiceClassesAspect(SessionFactory sessionFactory) {
+        this.hibernateStatistics = sessionFactory.getStatistics();
+    }
+
+    @Pointcut("execution(public * com.example.demowithtests.service..*.*(..))")
     public void callAtMyServicesPublicMethods() {
+    }
+
+    @Pointcut("execution(public * com.example.demowithtests.service.fillDataBase.LoaderServiceBean.*(..))")
+    public void callAtLoader() {
     }
 
     @Before("callAtMyServicesPublicMethods()")
     public void logBefore(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().toShortString();
         Object[] args = joinPoint.getArgs();
+        startTime = LocalDateTime.now();
         if (args.length > 0) {
             log.debug(ANSI_BLUE + "Service: " + methodName + " - start. Args count - {}" + ANSI_RESET, args.length);
         } else {
@@ -37,6 +52,7 @@ public class LoggingServiceClassesAspect {
     public void logAfter(JoinPoint joinPoint, Object returningValue) {
         String methodName = joinPoint.getSignature().toShortString();
         Object outputValue;
+        log.debug("Duration(in millis): " + Duration.between(startTime, LocalDateTime.now()).toMillis());
         if (returningValue != null) {
             if (returningValue instanceof Collection) {
                 outputValue = "Collection size - " + ((Collection<?>) returningValue).size();
@@ -49,5 +65,11 @@ public class LoggingServiceClassesAspect {
         } else {
             log.debug(ANSI_BLUE + "Service: " + methodName + " - end." + ANSI_RESET);
         }
+    }
+    @AfterReturning("callAtLoader()")
+    public void countSqlQueries() {
+        long queryCounter = hibernateStatistics.getPrepareStatementCount();
+        log.debug("SQL queries:" + queryCounter);
+        hibernateStatistics.clear();
     }
 }
